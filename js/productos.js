b16051253706cb319d4b262c8f953ce1b7aa59be
LGilardoni <91572690+LGilardoni@users.noreply.github.com>
2021-12-20 10:00:42 -0300

    Carrito de compras fix

--- js/productos.js
+++ js/productos.js
@@ -53,3 +53,120 @@ let msg = "";
 msg += `<p> Oops...un erreur : <br><i>${elt}<i><p>`;
 itemsElements.innerHTML = msg;
 }
+
+// Carrito de compras
+
+    // CLICK EN COMPRAR
+    const comprarButton = document.querySelector('.comprarButton');
+    comprarButton.addEventListener('click', comprarButtonClicked);
+
+    //CONTENEDOR PRINCIPAL DE CARRITO
+    const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
+
+    function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+
+    addItemToShoppingCart(itemTitle, itemPrice);
+    }
+
+    function addItemToShoppingCart(itemTitle, itemPrice) {
+        const elementsTitle = shoppingCartItemsContainer.getElementsByClassName('shoppingCartItemTitle');
+        for (let i = 0; i < elementsTitle.length; i++) {
+
+            if (elementsTitle[i].innerText === itemTitle) {
+            
+            let elementQuantity = elementsTitle[i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity');
+            elementQuantity.value++;
+            updateShoppingCartTotal();
+            $('.toast').toast('show'); // ANIMACIONES
+            return; //CORTA EL CICLO Y NO SE REPITE LA LÍNEA DE PRODUCTO
+            }
+        }
+
+        const shoppingCartRow = document.createElement('div');
+        const shoppingCartContent = 
+            `<div class="row shoppingCartItem">
+                <div class="col-4">
+                    <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                        <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                        <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div
+                        class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                        <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                            value="1">
+                        <button class="btn btn-danger buttonDelete" type="button">X</button>
+                    </div>
+                </div>
+            </div>`;
+        shoppingCartRow.innerHTML = shoppingCartContent;
+        shoppingCartItemsContainer.append(shoppingCartRow);
+
+        shoppingCartRow
+            .querySelector('.buttonDelete')
+            .addEventListener('click', removeShoppingCartItem);
+
+        shoppingCartRow
+            .querySelector('.shoppingCartItemQuantity')
+            .addEventListener('change', quantityChanged);
+
+        updateShoppingCartTotal();
+    }
+
+    function updateShoppingCartTotal() {
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+    shoppingCartItems.forEach((shoppingCartItem) => {
+
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$', ''));
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+
+    });
+
+    shoppingCartTotal.innerHTML = `$${total.toFixed(3)}`;
+
+    }
+
+    function removeShoppingCartItem(event) {
+
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+
+    }
+
+    function quantityChanged(event) {
+
+    const input = event.target;
+    input.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCartTotal();
+
+    }
+
+    //Funcion de click en realizar compra 
+    function comprarButtonClicked() {
+    shoppingCartItemsContainer.innerHTML = '';
+    updateShoppingCartTotal();
+
+    }
+
+    const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+    console.log(addToShoppingCartButtons);
+    addToShoppingCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+    });
