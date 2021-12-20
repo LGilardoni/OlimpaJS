9d8efbc8971bcc439e5b5ad271c5368a4d50ab27
LGilardoni <91572690+LGilardoni@users.noreply.github.com>
2021-12-20 09:40:07 -0300

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,10 @@
 // Scruikk dek navbar
 $(function () {
     $(window).on('scroll', function () {
-        if (($(window).scrollTop() >= 560 ) && (localStorage.getItem("dark-mode") === "false")) {
+        if ($(window).scrollTop() >= 560) {
             $('.navbar').addClass('active');
-            $('.navbar').removeClass('nav-dark');
         } 
-        else if (($(window).scrollTop() > 560 ) && (localStorage.getItem("dark-mode") === "true")) {
+        else if ($(window).scrollTop() > 560 ) {
             $('.navbar').addClass('active');
         } else {
             $('.navbar').removeClass('active');
